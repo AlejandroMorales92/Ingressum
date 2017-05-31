0a422b85c11d247f56374bf0f65afcf7f678bee7
EdyAguirre <joseaguirre9518@gmail.com>
2017-05-31 17:04:04 -0500

    Validaciones Formulario Nuevo colaborador
    Se agregaron las validaciones en el formulario
    Se agregaron las validaciones en el servidor

--- controller/class.agregarColaboradorController.php
+++ controller/class.agregarColaboradorController.php
@@ -15,17 +15,23 @@ class agregarColaborador extends controllerExtended {
       $colaborador->setDireccion($request->getParam('direccion'));
       $colaborador->setCorreo($request->getParam('correo'));
 
-      $colaboradorDAO = new colaboradorDAOExt($this->getConfig());
-      $respuesta1 = $colaboradorDAO->insert($colaborador);
-      $respuesta2 = array(
-          'code' => ($respuesta1 > 0) ? 200 : 500,
-          'datos' => $respuesta1
-      );
+      $validate = $this->validateData($colaborador);
+      if ($validate['code'] === 300) {
+        $this->setParam('rsp', $validate);
+        $this->setView('imprimirJson');
+      } else {
+        $colaboradorDAO = new colaboradorDAOExt($this->getConfig());
+        $respuesta1 = $colaboradorDAO->insert($colaborador);
+        $respuesta2 = array(
+            'code' => ($respuesta1 > 0) ? 200 : 500,
+            'datos' => $respuesta1
+        );
 
-      $this->setParam('rsp', $respuesta2);
-      $this->setView('imprimirJson');
+        $this->setParam('rsp', $respuesta2);
+        $this->setView('imprimirJson');
+      }
     } catch (Exception $exc) {
-      echo $exc->getMessage();
+      $this->showException($exc);
     }
   }
 
@@ -36,5 +42,81 @@ class agregarColaborador extends controllerExtended {
     require $this->getConfig()->getPath() . 'model/extended/class.colaboradorDAOExt.php';
   }
 
-}
+// Validaciones
+  private function validateData(colaborador $colaborador) {
+    $answer['code'] = 200;
+    $colaboradorDAO = new colaboradorDAOExt($this->getConfig());
 
+
+
+// Validación cedula
+    if (is_integer((integer) $colaborador->getCedula()) === false) {
+      $answer['code'] = 300;
+      $answer['datos']['cedula'] = 'La cédula no es un número válido';
+    } elseif (!preg_match('/^[0-9]+/', $colaborador->getCedula())) {
+      $answer['code'] = 300;
+      $answer['datos']['cedula'] = 'La cédula no es un número válido';
+    } elseif (count($colaboradorDAO->search($colaborador->getCedula())) > 0) {
+      $answer['code'] = 300;
+      $answer['datos']['cedula'] = 'El número de cédula ya existe !!!';
+    }
+
+// Validación nombre
+    $nombreLen = 100;
+    if (strlen($colaborador->getNombre()) > $nombreLen) {
+      $answer['code'] = 300;
+      $answer['datos']['nombre'] = 'El número máximo de caracteres permitidos es ' . $nombreLen;
+    } elseif (!preg_match('/^[a-z A-Z]+/', $colaborador->getNombre())) {
+      $answer['code'] = 300;
+      $answer['datos']['cedula'] = 'No es un nombre válido';
+    }
+
+// Validación apellido
+    $apellidoLen = 100;
+    if (strlen($colaborador->getApellido()) > $apellidoLen) {
+      $answer['code'] = 300;
+      $answer['datos']['apellido'] = 'El número máximo de caracteres permitidos es ' . $apellidoLen;
+    }
+
+
+// Validación categoria
+    $categoriaLen = 100;
+    if (strlen($colaborador->getCategoria()) > $categoriaLen) {
+      $answer['code'] = 300;
+      $answer['datos']['categoria'] = 'El número máximo de caracteres permitidos es ' . $categoriaLen;
+      // preg_match('/hombre|mujer|marikita feo/', $subject)
+    }
+
+
+
+// Validación telefono
+    $telefonoLen = 100;
+    if (strlen($colaborador->getTelefono()) > $telefonoLen) {
+      $answer['code'] = 300;
+      $answer['datos']['telefono'] = 'El número máximo de caracteres permitidos es ' . $telefonoLen;
+    }
+
+
+// Validación direccion
+    $direccionLen = 100;
+    if (strlen($colaborador->getDireccion()) > $direccionLen) {
+      $answer['code'] = 300;
+      $answer['datos']['direccion'] = 'El número máximo de caracteres permitidos es ' . $direccionLen;
+    }
+
+
+
+// Validación correo
+    $correoLen = 100;
+    if (strlen($colaborador->getCorreo()) > $correoLen) {
+      $answer['code'] = 300;
+      $answer['datos']['direccion'] = 'El número máximo de caracteres permitidos es ' . $correoLen;
+    } elseif (!filter_var($colaborador->getCorreo(), FILTER_VALIDATE_EMAIL)) {
+      $answer['code'] = 300;
+      $answer['datos']['correo'] = 'El correo no es válido';
+    }
+
+    return $answer;
+  }
+
+}
--- www/app/controller/controller.registroColaboradores.js
+++ www/app/controller/controller.registroColaboradores.js
@@ -1,114 +1,140 @@
-angular.module('IngressumApp').controller('registroColaboradoresController', ['$scope', 'registroColaboradorService', '$sessionStorage', '$location', 'rolAdmin', '$route', '$timeout', function ($scope, agregarColaborador, $sessionStorage, $location, rolAdmin, $route, $timeout) {
+angular.module('IngressumApp').controller('registroColaboradoresController', ['$scope', 'registroColaboradorService', '$sessionStorage', '$location', 'rolAdmin', '$route', '$timeout', '$parse', function ($scope, agregarColaborador, $sessionStorage, $location, rolAdmin, $route, $timeout, $parse) {
 
-        $scope.dataRegistrarColaborador = {
-            cedula: '',
-            nombre: '',
-            apellido: '',
-            categoria: '',
-            telefono: '',
-            direccion: '',
-            correo: ''
-        };
+    $scope.dataRegistrarColaborador = {
+      cedula: '',
+      nombre: '',
+      apellido: '',
+      categoria: '',
+      telefono: '',
+      direccion: '',
+      correo: ''
+    };
+
+    $scope.inputError = {
+      cedula: false,
+      nombre: false,
+      apellido: false,
+      categoria: false,
+      telefono: false,
+      direccion: false,
+      correo: false
+    };
+
+    $scope.colaboradorRegistrado = false;
+    $scope.colaboradorEditado = false;
+    $scope.colaboradorEliminado = false;
+    $scope.colaboradores = [];
+    $scope.edit = {};
+
+    $scope.pintarTabla = function () {
+      agregarColaborador.obtenerCol.then(function successCallback(response) {
+        switch (response.data.code) {
+          case 200:
+            $scope.colaboradores = response.data.datos;
+            break;
+          case 500:
+            console.log(response.data);
+            //$scope.colaboradores = [];
+        }
+      });
+    };
+
+    $scope.pintarTabla();
+
+    $scope.submit = function () {
+      agregarColaborador.agregarCol($scope.dataRegistrarColaborador).then(function successCallback(response) {
         $scope.colaboradorRegistrado = false;
+        $scope.dataRegistrarColaborador = {};
+        if (response.data.code == 300) {
+
+          angular.forEach(response.data.datos, function (value, key) {
+            $parse('inputError.' + key).assign($scope, value);
+          });
+
+          console.log(response);
+
+        } else if (response.data.code == 500) {
+          console.log(response.data.datos);
+        } else {
+          $scope.colaboradorRegistrado = true;
+          $scope.dataRegistrarColaborador = '';
+
+          $timeout(function () {
+            $('#nuevoColaborador').modal('toggle');
+          }, 700);
+          $timeout(function () {
+            // $route.reload();
+            window.location.reload();
+          }, 1000);
+
+        }
+      }, function errorCallback(response) {
+        console.error(response);
+      });
+    };
+
+
+
+
+    $scope.editar = function (dato) {
+      $scope.edit.id = dato.col_id;
+      $scope.edit.cedula = dato.col_cedula;
+      $scope.edit.nombre = dato.col_nombre;
+      $scope.edit.apellido = dato.col_apellido;
+      $scope.edit.categoria = dato.col_categoria;
+      $scope.edit.telefono = dato.col_telefono;
+      $scope.edit.direccion = dato.col_direccion;
+      $scope.edit.correo = dato.col_correo;
+      $('#editarColaborador').modal('toggle');
+    };
+
+    $scope.submitEditarColaborador = function () {
+      agregarColaborador.editarCol($scope.edit).then(function successCallback(response) {
         $scope.colaboradorEditado = false;
-        $scope.colaboradorEliminado = false;
-        $scope.colaboradores = [];
         $scope.edit = {};
-
-        $scope.pintarTabla = function () {
-            agregarColaborador.obtenerCol.then(function successCallback(response) {
-                switch (response.data.code) {
-                    case 200:
-                        $scope.colaboradores = response.data.datos;
-                        break;
-                    case 500:
-                        $scope.colaboradores = [];
-                }
-            });
-        };
-
-        $scope.pintarTabla();
-
-        $scope.submit = function () {
-            agregarColaborador.agregarCol($scope.dataRegistrarColaborador).then(function successCallback(response) {
-                $scope.colaboradorRegistrado = false;
-                $scope.dataRegistrarUsuario = {};
-                if (response.data.code == 500) {
-                } else {
-                    $scope.colaboradorRegistrado = true;
-                    $scope.dataRegistrarUsuario = '';
-                    $timeout(function () {
-                        $('#nuevoColaborador').modal('toggle');
-                    }, 700);
-                    $timeout(function () {
-                        // $route.reload();
-                        window.location.reload();
-                    }, 1000);
-                }
-            }, function errorCallback(response) {
-                console.error(response);
-            });
-        };
-        
-        
-        
-
-        $scope.editar = function (dato) {
-            $scope.edit.id = dato.col_id;
-            $scope.edit.cedula = dato.col_cedula;
-            $scope.edit.nombre = dato.col_nombre;
-            $scope.edit.apellido = dato.col_apellido;
-            $scope.edit.categoria = dato.col_categoria;
-            $scope.edit.telefono = dato.col_telefono;
-            $scope.edit.direccion = dato.col_direccion;
-            $scope.edit.correo = dato.col_correo;
+        if (response.data.code == 500) {
+        } else {
+          $scope.colaboradorEditado = true;
+          $scope.edit = '';
+          $timeout(function () {
             $('#editarColaborador').modal('toggle');
-        };
-        
-        $scope.submitEditarColaborador = function () {
-            agregarColaborador.editarCol($scope.edit).then(function successCallback(response) {
-                $scope.colaboradorEditado = false;
-                $scope.edit = {};
-                if (response.data.code == 500) {
-                } else {
-                    $scope.colaboradorEditado = true;
-                    $scope.edit = '';
-                    $timeout(function () {
-                        $('#editarColaborador').modal('toggle');
-                    }, 700);
-                    $timeout(function () {
-                        // $route.reload();
-                        window.location.reload();
-                    }, 1000);
-                }
-            }, function errorCallback(response) {
-                console.error(response);
-            });
-        };
-        
-        
-        $scope.eliminar = function (dato) {
+          }, 700);
+          $timeout(function () {
+            // $route.reload();
+            window.location.reload();
+          }, 1000);
+        }
+      }, function errorCallback(response) {
+        console.error(response);
+      });
+    };
+
+
+
+
+
+    $scope.eliminar = function (dato) {
+      $('#eliminarColaborador').modal('toggle');
+      $scope.nombre = dato.col_nombre;
+      $scope.ideliminar = dato.col_id;
+    };
+
+    $scope.submitEliminarColaborador = function () {
+      agregarColaborador.eliminarCol({id: $scope.ideliminar}).then(function successCallback(response) {
+        $scope.colaboradorEliminado = false;
+        if (response.data.code == 500) {
+        } else {
+          $scope.colaboradorEliminado = true;
+          $timeout(function () {
             $('#eliminarColaborador').modal('toggle');
-            $scope.nombre = dato.col_nombre;
-            $scope.ideliminar = dato.col_id;
-        };
-        
-        $scope.submitEliminarColaborador = function () {
-            agregarColaborador.eliminarCol({id: $scope.ideliminar}).then(function successCallback(response) {
-                $scope.colaboradorEliminado = false;
-                if (response.data.code == 500) {
-                } else {
-                    $scope.colaboradorEliminado = true;
-                    $timeout(function () {
-                        $('#eliminarColaborador').modal('toggle');
-                    }, 700);
-                    $timeout(function () {
-                        // $route.reload();
-                        window.location.reload();
-                    }, 1000);
-                }
-            }, function errorCallback(response) {
-                console.error(response);
-            });
-        };
-    }]);
+          }, 700);
+          $timeout(function () {
+            // $route.reload();
+            window.location.reload();
+          }, 1000);
+        }
+      }, function errorCallback(response) {
+        console.error(response);
+      });
+    };
+  }]);
